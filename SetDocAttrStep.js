4b7e56f7287755b733febd43e8081d23f0c37454
GeorgeFreeman <thenewsit@gmail.com>
2020-12-22 00:57:30 +0500

    add update parameter

--- SetDocAttrStep.js
+++ SetDocAttrStep.js
@@ -3,14 +3,16 @@ import has from 'lodash/has'
 import set from 'lodash/set'
 import unset from 'lodash/unset'
 import get from 'lodash/get'
+import isPlainObject from 'lodash/isPlainObject'
 
 const STEP_TYPE = 'SetDocAttrStep'
 
 export default class SetDocAttrStep extends Step {
-  constructor (path, value) {
+  constructor (path, value, update = false) {
     super()
     this.path = path
     this.value = value
+    this.update = update
   }
 
   get stepType () { return STEP_TYPE }
@@ -27,18 +29,21 @@ export default class SetDocAttrStep extends Step {
     }
 
     this.prevValue = has(doc.attrs, this.path) ? get(doc.attrs, this.path) : false
+    this.prevUpdate = !this.update
     // avoid clobbering doc.type.defaultAttrs
     if (doc.attrs === doc.type.defaultAttrs) doc.attrs = Object.assign({}, doc.attrs)
     if (!this.value) {
       unset(doc.attrs, this.path)
-    } else {
+    } else if (!this.update) {
       set(doc.attrs, this.path, this.value)
+    } else if (isPlainObject(this.prevValue)) {
+      set(doc.attrs, this.path, { ...this.prevValue, ...this.value })
     }
     return StepResult.ok(doc)
   }
 
   invert () {
-    return new SetDocAttrStep(this.path, this.prevValue)
+    return new SetDocAttrStep(this.path, this.prevValue, this.prevUpdate)
   }
 
   // position never changes so map should always return same step
@@ -48,12 +53,13 @@ export default class SetDocAttrStep extends Step {
     return {
       stepType: this.stepType,
       path: this.path,
-      value: this.value
+      value: this.value,
+      update: this.update
     }
   }
 
   static fromJSON (schema, json) {
-    return new SetDocAttrStep(json.path, json.value)
+    return new SetDocAttrStep(json.path, json.value, json.update)
   }
 }
 
