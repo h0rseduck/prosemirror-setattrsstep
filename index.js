5462b7d08c68b6f19f0e5ca7a30c711d9c0b8286
GeorgeFreeman <thenewsit@gmail.com>
2020-12-21 23:27:47 +0500

    update package.json

--- index.js
+++ index.js
@@ -1,3 +1,2 @@
 export { default as SetAttrsStep } from './SetAttrsStep'
 export { default as SetDocAttrStep } from './SetDocAttrStep'
-export { default as SetDocItemAttrStep } from './SetDocItemAttrStep'
